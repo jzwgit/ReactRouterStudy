199770636a857b8ec5b34cc1eac341ae3cd9b4a4
jzw <706861648@qq.com>
2018-12-29 15:20:56 +0800

    完成demo04-demo12的编写

--- router-demo01/src/App.js
+++ router-demo01/src/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import BasicExample from './demo01.js';
 import ParamsExample from './demo02.js';
 import AuthExample from './demo03.js';
+import CustomLinkExample from './demo04.js';
+import PreventingTransitionsExample from './demo05.js';
+import NoMatchExample from './demo06.js';
+import RecursiveExample from './demo07.js';
+import SidebarExample from './demo08.js';
+import AnimationExample from './demo09.js';
+import AmbiguousExample from './demo10.js';
+import RouteConfigExample from './demo11.js';
+import ModalGallery from './demo12.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-	<AuthExample />
+	<ModalGallery />
       </div>
     );
   }
